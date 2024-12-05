c017a7af996bd39343416df2b72c7c537a1108bf
ametzler <alexander.metzler@doubleslash.de>
2024-12-05 16:45:47 +0100

    initialized koin in iosMain

--- composeApp/src/iosMain/kotlin/de/doubleslash/cmpprototype/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/de/doubleslash/cmpprototype/MainViewController.kt
@@ -1,5 +1,9 @@
 package de.doubleslash.cmpprototype
 
 import androidx.compose.ui.window.ComposeUIViewController
+import de.doubleslash.cmpprototype.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    initKoin()
+    App()
+}
